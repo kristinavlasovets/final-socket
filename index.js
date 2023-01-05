77a16a4dfaf766cf2cd6fc0deb175ffef93817ea
Kristina Vlasovets <kristinavlasovets@gmail.com>
2023-01-05 13:14:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 8900;
+
+const io = require('socket.io')(PORT, {
+	cors: {
+		origin: process.env.CLIENT_URL,
+	},
+});
+io.on('connection', (socket) => {
+	console.log('User is connected');
+	socket.on('sendComment', ({name, userId, reviewId, desc}) => {
+		io.emit('getComment', {name, userId, reviewId, desc});
+	});
+
+	socket.on('disconnect', () => {
+		console.log('User is disconnected');
+	});
+});
